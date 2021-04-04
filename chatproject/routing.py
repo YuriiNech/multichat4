dd244b718423c0789d8a148fc7dc8ecf26b0bed6
Yurii Nechytailo <jurij_N@rambler.ru>
2021-04-04 19:16:21 +0300

    Update routing.py

--- chatproject/routing.py
+++ chatproject/routing.py
@@ -1,6 +1,9 @@
-from channels.routing import route
-
+# from channels.routing import route
+from channels.staticfiles import StaticFilesConsumer
+# from chatapp import consumers
 channel_routing ={
+    'http.request': StaticFilesConsumer(),
+
     'websocket.connect': 'chatapp.consumers.ws_connect',
     'websocket.receive': 'chatapp.consumers.ws_message',
     'websocket.disconnect': 'chatapp.consumers.ws_disconnect',
