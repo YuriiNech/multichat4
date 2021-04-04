c8d02da232e92f1690510837584ecd8232fbfd42
Yurii Nechytailo <jurij_N@rambler.ru>
2021-04-04 19:13:49 +0300

    Update wsgi.py

--- chatproject/wsgi.py
+++ chatproject/wsgi.py
@@ -1,8 +1,7 @@
+  
 """
-WSGI config for chat2 project.
-
+WSGI config for chat4 project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
@@ -10,7 +9,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatproject.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
