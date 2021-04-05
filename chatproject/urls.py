897605bf1eed40ea46cead2dbdf273322a7aea91
Yurii Nechytailo <jurij_N@rambler.ru>
2021-04-05 22:48:15 +0300

    Update urls.py

--- chatproject/urls.py
+++ chatproject/urls.py
@@ -1,27 +1,11 @@
-"""chat2 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 from chatapp.views import chat, register, accounts, logout_view, get_privat_chat, create_privat_chat, privat_chat
-from chatapp.views import is_username_exists, change_password, leave_chat, users, show, clear_db
-
-
+from chatapp.views import is_username_exists, change_password, leave_chat, users, show, clear_db, about
 
 urlpatterns = [
+    url(r'^about/', about),
     url(r'^$', chat),
     url(r'^admin/', admin.site.urls),
     url(r'^chat/', chat),
@@ -31,7 +15,7 @@ urlpatterns = [
     url(r'^register/', register),
     url(r'^login/', login),
     url(r'^logout/', logout_view),
-    url(r'^accounts/profile/', accounts),
+    url(r'^accounts/profile/', chat),
     url(r'^is_username_exists/', is_username_exists),
     url(r'^change_password/', change_password),
     url(r'^leave/', leave_chat),
